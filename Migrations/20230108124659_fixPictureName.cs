a84830f988745a5f146ff4d2d3c86c8e6980d6c1
Oleg Kotuk <131190655+OlegKotuk@users.noreply.github.com>
2024-04-11 01:20:29 +0300

    Add files via upload

---
+++ Migrations/20230108124659_fixPictureName.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eTickets.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixPictureName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfilePicureURL",
+                table: "Producers",
+                newName: "ProfilePictureURL");
+
+            migrationBuilder.RenameColumn(
+                name: "ProfilePicureURL",
+                table: "Actors",
+                newName: "ProfilePictureURL");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Movies",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfilePictureURL",
+                table: "Producers",
+                newName: "ProfilePicureURL");
+
+            migrationBuilder.RenameColumn(
+                name: "ProfilePictureURL",
+                table: "Actors",
+                newName: "ProfilePicureURL");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
